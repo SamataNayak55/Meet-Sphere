7a63fd3237ca1aa1c53a2ef547647c94673caa66
samata nayak <samata.nayak55@gmail.com>
2025-12-04 17:42:32 +0530

    game  page

--- frontend/src/pages/videomeet.jsx
+++ frontend/src/pages/videomeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 
-const server_url = "https://meet-sphere-frontend.vercel.app/";
+const server_url = "https://meet-sphere-peach.vercel.app";
 
 var connections = {};
 
