2bc15f8d0ddf9e0d8627855bc9ab275712ccc464
samata nayak <samata.nayak55@gmail.com>
2025-12-04 17:27:02 +0530

    game  page

--- frontend/src/pages/Todo.jsx
+++ frontend/src/pages/Todo.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useEffect } from "react";
-import '../styles/todo.css'
+import '../Styles/todo.css'
 import withAuth from "../utils/withAuth";
 
  export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
