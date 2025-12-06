fa0d77ca69ea08c65e48eb3aaf8d7f08229f9ebf
samata nayak <samata.nayak55@gmail.com>
2025-12-06 20:44:27 +0530

    rsolve errors of deployment

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -8,7 +8,7 @@ let timeOnline = {}
 export const connectToSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "https://meet-sphere-peach.vercel.app",
+            origin: "https://s-meet-sphere.vercel.app",
             methods: ["GET", "POST"],
             allowedHeaders: ["*"],
             credentials: true
