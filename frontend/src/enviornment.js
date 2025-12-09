3dc7c3f8044ece22babde504c1bec789b28fcf2d
samata nayak <samata.nayak55@gmail.com>
2025-12-09 11:38:03 +0530

    small change

--- frontend/src/enviornment.js
+++ frontend/src/enviornment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://meet-sphere-backend.vercel.app" :
+    "https://meet-sphere-backend.onrender.com" :
     "http://localhost:8000"
 
 export default server;
